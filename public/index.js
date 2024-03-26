193e2a645ceb2e82accef09271f15dae489d463a
Andika MF <irukainu20@gmail.com>
2024-03-27 01:18:38 +0700

    add : timer

--- public/index.js
+++ public/index.js
@@ -1,21 +1,67 @@
-const requestOptions = {
-    method: "GET",
-    redirect: "follow"
-};
+const apiUrl =
+  "https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=1JXW7D1CUKX4HM1E9IX4U62Z4Z36NX35DT";
 
-    fetch("https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=1JXW7D1CUKX4HM1E9IX4U62Z4Z36NX35DT", requestOptions)
-    .then(response => response.json())
-    .then(data => {
-        const safeGasPrice = data.result.SafeGasPrice;
-        const proposeGasPrice = data.result.ProposeGasPrice;
-        const fastGasPrice = data.result.FastGasPrice;
+const timerDisplay = document.getElementById("timer");
 
-        document.getElementById('safeGasPrice').innerText = `Gwei : ${safeGasPrice}`;
-        document.getElementById('proposeGasPrice').innerText = `Gwei : ${proposeGasPrice}`;
-        document.getElementById('fastGasPrice').innerText = `Gwei : ${fastGasPrice}`;
-    })
-    .catch(error => console.error('Error fetching gas data:', error));
+function resetTimer() {
+  let seconds = 10;
 
-fetchDataAndUpdateUI();
+  updateTimerDisplay(seconds);
 
-setInterval(fetchDataAndUpdateUI, 60000);
+  const countdown = setInterval(() => {
+    seconds--;
+
+    updateTimerDisplay(seconds);
+
+    if (seconds === 0) {
+      clearInterval(countdown);
+
+      resetTimerAndFetchData();
+    }
+  }, 1000);
+}
+
+function updateTimerDisplay(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  const display = `${minutes < 10 ? "0" : ""}${minutes}:${
+    remainingSeconds < 10 ? "0" : ""
+  }${remainingSeconds}`;
+
+  timerDisplay.textContent = `Next Update in : ${display}`;
+}
+
+async function fetchDataAndUpdateHTML() {
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data from API");
+    }
+
+    const data = await response.json();
+
+    const { SafeGasPrice, ProposeGasPrice, FastGasPrice } = data.result;
+
+    document.getElementById(
+      "safeGasPrice"
+    ).innerText = `Gwei : ${SafeGasPrice}`;
+    document.getElementById(
+      "proposeGasPrice"
+    ).innerText = `Gwei : ${ProposeGasPrice}`;
+    document.getElementById(
+      "fastGasPrice"
+    ).innerText = `Gwei : ${FastGasPrice}`;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+function resetTimerAndFetchData() {
+  resetTimer();
+
+  fetchDataAndUpdateHTML();
+}
+
+resetTimerAndFetchData();
